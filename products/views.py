1d191ad4c59bd3027a3f186e94d83108f6212c4a
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-07-15 17:56:07 +0100

    add serach functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.contrib import messages
+from django.shortcuts import render, redirect,reverse, get_object_or_404
+from django.db.models import Q
 from products.models import Product
 
 
@@ -9,7 +10,18 @@ def all_products(request):
     '''A view to show all products page, including sorting and search queries'''
 
     products = Product.objects.all()
-    context = {'products': products}
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'Please enter a search query')
+                return redirect(reverse('products'))
+
+            queries=Q(name__icontains=query)|Q(description__icontains=query)
+            products=products.filter(queries)
+
+    context = {'products': products,'search_term':query}
 
     return render(request,'products/products.html', context)
 
