7b3cf88b4133e186b512c6c552a17b1068a7e789
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-07-28 23:08:29 +0100

    add stripe payment intent to checkout view

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -171,3 +173,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 FREE_DELIVERY_THRESHOLD= 50
 STANDARD_DELIVERY_PERCENTAGE= 10
+STRIPE_CURRENCY='USD'
+STRIPE_PUBLIC_KEY=os.getenv('STRIPE_PUBLIC_KEY','')
+STRIPE_SECRET_KEY=os.getenv('STRIPE_SECRET_KEY','')
