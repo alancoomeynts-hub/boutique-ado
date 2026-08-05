4062af5ac31c07150cbd208cb984d11f8c1f412a
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-08-05 22:18:57 +0100

    Checkout app finalised

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-0_x7pj*1)2l*5y#1&f6q+62f(g!hp)x*6&%56aug8_0^p737wa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -35,3 +35,15 @@ def webhook(request):
 
     except Exception as e:
         return HttpResponse(content=e, status=400)
+
+    handler = StripeWH_Handler(request)
+
+    event_map={
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    event_type=event['type']
+    event_handler=event_map.get(event_type,handler.handle_event)
+    response=event_handler(event)
+    return response
