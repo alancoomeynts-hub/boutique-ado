04ef916e7289e8bf1825cb8a16d9d8bf8d6c99f9
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-08-13 23:08:54 +0100

    Push Deployment Settings for static and media files

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -54,8 +54,10 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
 
+
     # Other
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -181,6 +183,25 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+#bucket config
+if 'USE_AWS' in os.environ:
+    AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME="boutique-ado-ac"
+    AWS_S3_REGION_NAME="Europe (Stockholm) eu-north-1"
+    AWS_S3_CUSTOM_DOMAIN=f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    #static storage
+    STATICFILES_STORAGE='custom_storages.StaticStorage'
+    STATICFILES_LOCATION='static'
+    #media storage
+    DEFAULT_FILE_STORAGE='custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION='media'
+
+    STATIC_URL=f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL=f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 FREE_DELIVERY_THRESHOLD= 50
 STANDARD_DELIVERY_PERCENTAGE= 10
