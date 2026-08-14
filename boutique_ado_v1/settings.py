78a3d342b0aed5d285b1ddf145cf56e02fe6d5fe
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-08-14 16:37:27 +0100

    Push Deployment Settings

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -186,6 +186,13 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 #bucket config
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+
     AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME="boutique-ado-ac"
